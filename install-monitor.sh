55f8d065557a93fcbd61edfe3c6e4d55553cf555
lwqlwq60 <liuweiqian01@163.com>
2021-09-02 11:33:16 +0800

    Update install-monitor.sh

--- install-monitor.sh
+++ install-monitor.sh
@@ -1,3 +1,16 @@
+mkdir /opt/crust-monitor
+
+if [ ! -f /opt/crust-monitor/nodes.txt ]; then
+	cat > nodes.txt
+fi
+
+if [ ! -f /opt/crust-monitor/addresses.txt ]; then
+	cat > addresses.txt
+fi
+
+if [ ! -f /opt/crust-monitor/mails.txt ]; then
+	cat > mails.txt
+fi
 cp appsettings.json /opt/crust-monitor/appsettings.json
 
 docker run -p 80:80 -v /opt/crust-monitor/appsettings.json:/app/appsettings.json -v /opt/crust-monitor/mails.txt:/app/mails.txt -v /opt/crust-monitor/nodes.txt:/app/nodes.txt -v /opt/crust-monitor/addresses.txt:/app/addresses.txt --name="crust-monitor" --restart always -d lwqlwq60/crust-monitor:latest
